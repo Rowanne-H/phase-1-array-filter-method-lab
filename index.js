aa46042ab0cfbce8a3203a717e55e117dcabad99
YUERONG HUANG <rowanne.h@hotmail.com>
2024-01-15 21:45:47 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, name) {
+    return arr.filter(e => e.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(arr, letters) {
+    return arr.filter(e => e.indexOf(letters) === 0)
+}
+
+function matchName(arr, name) {
+    return arr.filter(e => e.name === name)
+}
